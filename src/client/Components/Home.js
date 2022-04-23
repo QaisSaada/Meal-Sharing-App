803b11243a5d6bc144ebc2d1889ab5b73445c460
qaissaada <qais.m.saada@gmail.com>
2022-04-23 12:57:58 +0200

    meal sharing app frontend

---
+++ src/client/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../StyleComp/Navbar';
+import Footer from '../StyleComp/Footer';
+
+function Home() {
+  return (
+<div>   
+<Navbar />
+       
+<Footer /> 
+</div>   
+  )
+}
+
+export default Home;
