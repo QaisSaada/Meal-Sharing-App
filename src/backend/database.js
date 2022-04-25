76f7ff6da6fbc6583ca89aa720e832fb8f42260e
qaissaada <qais.m.saada@gmail.com>
2022-04-25 18:25:44 +0200

    edit2

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
-
+console.log('mamamia',process.env.DB_NAME,process.env.DB_USER )
 // create connection
-const knex = require("knex")({
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-  pool: { min: 0, max: 7 },
-});
+let knex;
+try {
+  knex = require("knex")({
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: { min: 0, max: 7 },
+  });
+} catch (error) {console.log(error)}
+
 
 // Check that the connection works
 knex.raw("SELECT VERSION()").then(() => {
