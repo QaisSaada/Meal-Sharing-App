43f492e1536810480d9df961c8504e9f89bf8be3
qaissaada <qais.m.saada@gmail.com>
2022-05-04 15:52:14 +0200

    fixed 1- function cannot be child of react 2- fetch url 3- useParams was missing 4- parsing jsonwas incorrect

--- src/client/Components/Meal.js
+++ src/client/Components/Meal.js
@@ -1,42 +1,52 @@
 import React, { useState, useEffect } from 'react';
+import { Link, useParams  } from 'react-router-dom';
 
-const Meal = () => (/* {match} */) => {
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
+
+const Meal = () => {
+  const [data, setData] = useState([]);
+  let {id} = useParams();
+
+  useEffect(() => {
     fetchMeal();
-    }, []);
+  }, []);
 
   const fetchMeal = () => {
-    fetch(`http://localhost:3000/api/meals/${match.params.id}` , {method: "GET", headers: {'Content-Type': 'application/json'}
-    }).then((res) => {
-          setData(res.data);
-          console.log(res.data);
+    fetch(`http://localhost:3000/api/meals/${id}` , {method: "GET", headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then((data) => {
+          setData(data);
+          console.log(data);
         })
         .catch((err) => console.log(err));
-    };
+    }
+
   return (
     <div>
-      {data.map((meal) => {
+      {data.map((data) => {
         return (
-          <div className='meal-container' key={meal.id}>
+          <div className='meal-container' key={data.id}>
             <div>
               <img className='meal-image' src={""} alt='' />
             </div>
             <div>
-              <h1 className='Food'>{meal.title}</h1>
-              <h2>{meal.meal}</h2>
-              <p>{meal.description}</p>
+              <h1 className='Food'>{data.title}</h1>
+              <h2>{data.meal}</h2>
+              <p>{data.description}</p>
               <p>
-                <strong>Price:</strong> {meal.price}
+                <strong>Price:</strong> {data.price}
               </p>
               <p>
-                <strong>When:</strong> {meal.whenn}
+                <strong>When:</strong> {data.whenn}
               </p>
             </div>
           </div>
         );
       })}
+      <div className='back'>
+        <Link to='/meal'>Featured meals</Link>
+      </div>
     </div>
   );
   };
