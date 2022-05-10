1bbae1b0ec5d7c8c8894120e584d5ad13ed51e5f
qaissaada <qais.m.saada@gmail.com>
2022-05-10 19:08:07 +0200

    reverting back frontend files

---
+++ src/backend/database.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+console.log('MAMAMIA', process.env.DB_PASSWORD, process.env.DB_NAME,)
+// create connection
+let knex;
+try {
+  knex = require("knex")({
+
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: { min: 0, max: 7 },
+  });
+} catch (error ) {
+  console.log(error)
+}
+
+
+// Check that the connection works
+knex.raw("SELECT VERSION()").then(() => {
+  console.log(`connection to db successful!`);
+});
+
+module.exports = knex;
---
+++ src/client/Components/Update.js
@@ -0,0 +1,77 @@
+import React, {useState, useEffect} from 'react'
+import { Form, Checkbox, Button } from 'semantic-ui-react'; 
+import '../StyleComp/Update.css'; 
+
+
+const Update = (props) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [checkbox, setCheckbox] = useState(false); 
+    const [location, setLocation] = useState('');
+    const [price, setPrice] = useState('');
+    const [maxreservations, setMaxreservations] = useState(null);
+    const [createddate, setCreateddate] = useState(new Date())
+    const [whenn, setWhenn] = useState(new Date()) 
+    const [id, setID] = useState(null)
+
+    
+    useEffect(() => {
+        setID(localStorage.getItem('ID'))
+        setTitle(localStorage.getItem('Title'));
+        setDescription(localStorage.getItem('Description'));
+        setLocation(localStorage.getItem('Location'));
+        setPrice(localStorage.getItem('Price'));
+        setMaxreservations(localStorage.getItem('Max reservation'));
+        setCreateddate(localStorage.getItem('Created date'))
+        setWhenn(localStorage.getItem('Whenn'))
+}, []);
+
+    const updateAPIData = () => {
+        fetch(props.api + "/api/meals/", {method: "PUT", body: JSON.stringify({title,
+            description,
+            location,
+            price,
+            maxreservations,
+            createddate,
+            whenn,
+            checkbox})
+        }).then(res => console.log(res)).catch(err => console.log(err))
+    }
+    return( 
+    <Form className="update-form">
+        <Form.Field>
+            <label>Title</label>
+            <input placeholder='Title' onChange={(e) => setTitle(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Description</label>
+            <input placeholder='description' onChange={(e) => setDescription(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Location</label>
+            <input placeholder='Location' onChange={(e) => setLocation(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Price</label>
+            <input placeholder='Price' onChange={(e) => setPrice(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Maxreservations</label>
+            <input placeholder='Maxreservations' onChange={(e) => setMaxreservations(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Created date</label>
+            <input placeholder='Created date' onChange={(e) => setCreateddate(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>When </label>
+            <input placeholder='When' onChange={(e) => setWhenn(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <Checkbox onChange={(e) => setCheckbox(!checkbox)} label='I agree to the Terms and Conditions' />
+        </Form.Field>
+        <Button type='submit' checked={checkbox} onClick={updateAPIData} >Update</Button>
+    </Form>
+)}
+
+export default Update;
